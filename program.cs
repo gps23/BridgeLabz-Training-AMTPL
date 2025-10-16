17e39957d2d3c784857b9529cbc2a031f002ad5d
gps23 <143864435+gps23@users.noreply.github.com>
2025-10-16 16:05:06 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace BridgeLabzConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            Console.Write("Age:" + age);
+
+
+            float height = 5.7f;
+            Console.WriteLine("Height:" + height);
+
+            double weight = 60.5;
+            Console.WriteLine("Weight:" + weight);
+
+            var city = "Chennai";
+            Console.WriteLine("City:" + city);
+
+            Console.WriteLine("\n--- Press any key to exit --");
+            Console.ReadKey();
+        }
+
+    }
+}
