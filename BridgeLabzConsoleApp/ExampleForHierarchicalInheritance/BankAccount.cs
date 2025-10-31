8a0e0f153ce85899138492a115bea18aa3b756db
G Prajasree <prajasree230@gmail.com>
2025-10-31 11:23:01 +0530

    Applied .gitignore rules and cleaned tracked build files

---
+++ BridgeLabzConsoleApp/ExampleForHierarchicalInheritance/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BridgeLabzConsoleApp.ExampleForHierarchicalInheritance
+{
+    class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+
+        public BankAccount(int accountNumber, double balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}, Balance: {Balance:C}");
+        }
+    }
+}
