aec862dc5a5ac712318d3df226ff99386d3c741c
G Prajasree <prajasree230@gmail.com>
2025-10-31 11:23:01 +0530

    Applied .gitignore rules and cleaned tracked build files

---
+++ BridgeLabzConsoleApp/ExampleForSingleLevelInheritance/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzConsoleApp.ExampleForSingleLevelInheritance
+{
+    internal class Cat : Animal
+    {
+        public Cat(string name, int age) : base(name, age) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bird chirps");
+        }
+    }
+}
